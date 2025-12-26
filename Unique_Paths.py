2363859a80a36b27d07c6edc932effed4b9a25c2
TheGreatPratyush <pratushgupta27199@gmail.com>
2025-12-27 02:31:19 +0600

    LeetCode: Unique Paths

--- Unique_Paths.py
+++ Unique_Paths.py
@@ -1,4 +1,13 @@
 # Unique Paths
-# Language: Python
+# Platform: LeetCode
+# Language: Python3
 
-m x n
+class Solution:
+    def generate_subsequence(self,index,nums,subset,result,k):
+        if len(subset)==k:
+            result.append(subset[:])
+            return 
+        if index>=len(nums):
+            return 
+        subset.append(nums[index])
+        self.generate_subsequence(index+1,
